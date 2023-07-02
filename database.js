f1542c7ab4fa9309ce4ffef06faca86249281f95
Eduard Krasnov <eduard.krasnov1@hm.edu>
2023-07-02 15:44:53 +0200

    databse updated

--- database.js
+++ database.js
@@ -53,17 +53,26 @@ const data = [
         artist: "Franz Marc",
         date: "1912",
         img: "https://www.lenbachhaus.de/fileadmin/user_upload/images/G_13320%23%23MarcF_A1_ONLINE.jpg",
-        description: `Schau mal, hier ist das tolle Gemälde "Der Tiger" von einem Künstler namens Franz Marc! Schau dir den Tiger an, er ist so groß und stark! Das Bild ist mit leuchtenden Farben gemalt und der Tiger sieht wirklich beeindruckend aus.
-      Der Künstler wollte uns zeigen, wie mächtig und majestätisch Tiger sind. Schau dir seine leuchtenden blauen und grünen Farben an! Sie machen den Tiger noch aufregender und zeigen, wie stark er ist.
-      Stell dir vor, du könntest mit dem Tiger in den Dschungel gehen! Was würdest du alles erleben? Du könntest durch den Wald rennen und versteckte Tiere entdecken. Vielleicht würdest du bunte Vögel sehen, die hoch in den Bäumen singen. Und du könntest sogar andere wilde Tiere wie Löwen und Affen treffen!
-      Der Tiger wäre dein Freund und Beschützer im Dschungel. Er würde dir helfen, den Weg zu finden und auf dich aufpassen. Du würdest dich sicher und abenteuerlustig fühlen!
-      Das Gemälde "Der Tiger" von Franz Marc erzählt uns eine Geschichte von einem mächtigen Tier und lädt uns ein, in eine Welt der Fantasie und Abenteuer einzutauchen. Es erinnert uns auch daran, wie wichtig es ist, Tiere und die Natur zu schützen.
-      Wie fühlst du dich, wenn du den Tiger siehst? Welche Abenteuer würdest du mit ihm erleben? Lass deiner Fantasie freien Lauf und denke an all die spannenden Dinge, die du mit dem Tiger im Dschungel erleben könntest!
-      `,
+        description: `Schau mal, hier ist das tolle Gemälde "Der Tiger" von einem Künstler namens Franz Marc! Schau dir den Tiger an, er ist so groß und stark! Das Bild ist mit leuchtenden Farben gemalt und der Tiger sieht wirklich beeindruckend aus. \n Der Künstler wollte uns zeigen, wie mächtig und majestätisch Tiger sind. Schau dir seine leuchtenden blauen und grünen Farben an! Sie machen den Tiger noch aufregender und zeigen, wie stark er ist. \n Stell dir vor, du könntest mit dem Tiger in den Dschungel gehen! Was würdest du alles erleben? Du könntest durch den Wald rennen und versteckte Tiere entdecken. Vielleicht würdest du bunte Vögel sehen, die hoch in den Bäumen singen. Und du könntest sogar andere wilde Tiere wie Löwen und Affen treffen! \n Der Tiger wäre dein Freund und Beschützer im Dschungel. Er würde dir helfen, den Weg zu finden und auf dich aufpassen. Du würdest dich sicher und abenteuerlustig fühlen! \n Das Gemälde "Der Tiger" von Franz Marc erzählt uns eine Geschichte von einem mächtigen Tier und lädt uns ein, in eine Welt der Fantasie und Abenteuer einzutauchen. Es erinnert uns auch daran, wie wichtig es ist, Tiere und die Natur zu schützen. \n Wie fühlst du dich, wenn du den Tiger siehst? Welche Abenteuer würdest du mit ihm erleben? Lass deiner Fantasie freien Lauf und denke an all die spannenden Dinge, die du mit dem Tiger im Dschungel erleben könntest!`,
       },
       {
         title: "Blaues Pferd",
         artist: "Franz Marc",
+        date: "1911",
+        img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Franz_Marc_Blaues_Pferd_1911.jpg",
+        description: `Schau dir dieses besondere Gemälde namens "Blaues Pferd I" von Franz Marc an! Es zeigt ein Pferd, aber nicht in den üblichen Farben, sondern in einem leuchtenden Blau. Der Künstler, Franz Marc, hatte eine ganz eigene Art, Tiere zu malen und ihnen ungewöhnliche Farben zu geben.
+        Das Pferd in diesem Bild hat eine majestätische Erscheinung. Es steht ruhig da und wirkt fast wie aus einer anderen Welt. Sein Fell schimmert in einem wunderschönen Blauton, der uns verzaubert. Die Konturen des Pferdes sind weich und fließend, fast so, als würde es mit den Farben verschmelzen.
+        Wenn du genau hinschaust, wirst du feststellen, dass das Pferd nicht realistisch dargestellt ist. Es hat lange, geschwungene Linien und abstrakte Formen. Das gibt dem Bild etwas Besonderes, fast wie ein Traum oder eine Fantasie.
+        Der Hintergrund des Bildes ist eher einfach gehalten. Es gibt keine genauen Details, sondern nur eine sanfte Farbfläche. Es könnte der Himmel sein, in dem das Pferd steht, oder auch eine offene Landschaft. Die Farbe des Hintergrunds passt gut zum blauen Pferd und verstärkt seine Präsenz.
+        Dieses Gemälde regt unsere Fantasie an. Wenn du das Bild betrachtest, welche Gedanken und Gefühle kommen in dir auf? Vielleicht erinnert dich das Blau des Pferdes an den Ozean oder den Himmel an einem sonnigen Tag. Es könnte dich an Glück und Freiheit erinnern oder an eine magische Welt, in der alles möglich ist.
+        Franz Marc wollte uns mit diesem Bild zum Nachdenken anregen. Er wollte uns zeigen, dass Kunst verschiedene Emotionen und Ideen in uns hervorrufen kann. Jeder kann seine eigenen Gedanken und Gefühle beim Betrachten des Bildes haben.
+        Jetzt lade ich dich ein, deine eigenen Gedanken zu diesem Gemälde zu entdecken. Was denkst du, wenn du das blaue Pferd betrachtest? Welche Geschichten könntest du dir dazu ausdenken? Vielleicht hat das Pferd einen Namen und lebt in einer geheimnisvollen Welt voller Magie?
+        Du kannst deine eigene Fantasie verwenden, um das Bild zum Leben zu erwecken. Du könntest eine Geschichte schreiben oder ein Gedicht über das Pferd und seine Abenteuer. Oder du könntest selbst ein Bild malen, inspiriert von diesem blauen Pferd.
+        Kunst ist eine wunderbare Möglichkeit, deine Gedanken und Gefühle auszudrücken. Also lass deiner Kreativität freien Lauf und entdecke die faszinierende Welt dieses blauen Pferdes!`,
+      },
+      {
+        title: "Impression III (Konzert)",
+        artist: "Wassily Kandinsky",
         date: "1911",
         img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Franz_Marc_Blaues_Pferd_1911.jpg",
         description: `Schau dir dieses besondere Gemälde namens "Blaues Pferd I" von Franz Marc an! Es zeigt ein Pferd, aber nicht in den üblichen Farben, sondern in einem leuchtenden Blau. Der Künstler, Franz Marc, hatte eine ganz eigene Art, Tiere zu malen und ihnen ungewöhnliche Farben zu geben.
