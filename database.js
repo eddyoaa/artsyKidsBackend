2f74078a45b390810f0e50ac8476c2e6c14376db
Eduard Krasnov <eduard.krasnov1@hm.edu>
2023-07-02 15:49:23 +0200

    database update

--- database.js
+++ database.js
@@ -74,16 +74,30 @@ const data = [
         title: "Impression III (Konzert)",
         artist: "Wassily Kandinsky",
         date: "1911",
-        img: "https://upload.wikimedia.org/wikipedia/commons/8/8b/Franz_Marc_Blaues_Pferd_1911.jpg",
-        description: `Schau dir dieses besondere Gemälde namens "Blaues Pferd I" von Franz Marc an! Es zeigt ein Pferd, aber nicht in den üblichen Farben, sondern in einem leuchtenden Blau. Der Künstler, Franz Marc, hatte eine ganz eigene Art, Tiere zu malen und ihnen ungewöhnliche Farben zu geben.
-        Das Pferd in diesem Bild hat eine majestätische Erscheinung. Es steht ruhig da und wirkt fast wie aus einer anderen Welt. Sein Fell schimmert in einem wunderschönen Blauton, der uns verzaubert. Die Konturen des Pferdes sind weich und fließend, fast so, als würde es mit den Farben verschmelzen.
-        Wenn du genau hinschaust, wirst du feststellen, dass das Pferd nicht realistisch dargestellt ist. Es hat lange, geschwungene Linien und abstrakte Formen. Das gibt dem Bild etwas Besonderes, fast wie ein Traum oder eine Fantasie.
-        Der Hintergrund des Bildes ist eher einfach gehalten. Es gibt keine genauen Details, sondern nur eine sanfte Farbfläche. Es könnte der Himmel sein, in dem das Pferd steht, oder auch eine offene Landschaft. Die Farbe des Hintergrunds passt gut zum blauen Pferd und verstärkt seine Präsenz.
-        Dieses Gemälde regt unsere Fantasie an. Wenn du das Bild betrachtest, welche Gedanken und Gefühle kommen in dir auf? Vielleicht erinnert dich das Blau des Pferdes an den Ozean oder den Himmel an einem sonnigen Tag. Es könnte dich an Glück und Freiheit erinnern oder an eine magische Welt, in der alles möglich ist.
-        Franz Marc wollte uns mit diesem Bild zum Nachdenken anregen. Er wollte uns zeigen, dass Kunst verschiedene Emotionen und Ideen in uns hervorrufen kann. Jeder kann seine eigenen Gedanken und Gefühle beim Betrachten des Bildes haben.
-        Jetzt lade ich dich ein, deine eigenen Gedanken zu diesem Gemälde zu entdecken. Was denkst du, wenn du das blaue Pferd betrachtest? Welche Geschichten könntest du dir dazu ausdenken? Vielleicht hat das Pferd einen Namen und lebt in einer geheimnisvollen Welt voller Magie?
-        Du kannst deine eigene Fantasie verwenden, um das Bild zum Leben zu erwecken. Du könntest eine Geschichte schreiben oder ein Gedicht über das Pferd und seine Abenteuer. Oder du könntest selbst ein Bild malen, inspiriert von diesem blauen Pferd.
-        Kunst ist eine wunderbare Möglichkeit, deine Gedanken und Gefühle auszudrücken. Also lass deiner Kreativität freien Lauf und entdecke die faszinierende Welt dieses blauen Pferdes!`,
+        img: "https://assets.deutschlandfunk.de/FILE_96ac4bf9166aef487dfa5f8feda65298/768x576.jpg?t=1613821432874",
+        description: `"Impression III (Konzert)" ist ein besonderes Bild, das ein Konzert zeigt. Der Künstler, Wassily Kandinsky, war auf einem Konzert mit neuer Musik und war davon sehr beeindruckt. Er hat sogar Briefe mit dem Komponisten ausgetauscht und sie haben sich gegenseitig inspiriert. Am Ende hat der Komponist seine eigenen Bilder in einer Ausstellung gezeigt.
+        Kandinsky nannte dieses Bild "Impression", weil es seine direkten Eindrücke von der Natur widerspiegelt. Es geht nicht nur um das, was er sieht, sondern auch um andere Sinneswahrnehmungen. Er hat versucht, diese Eindrücke auf eine andere Art und Weise darzustellen. "Impression III (Konzert)" ist wie ein Gemälde von Klängen und Tönen.
+        Das Bild hat viel Gelb und Schwarz, und auch etwas Weiß. Das Schwarz ist wie ein Dreieck oben im Bild, und darunter ist das Gelb in einer fließenden Bewegung. Es scheint hin und her zu schwingen und zieht dabei die bunten Flecken und Linien an.
+        Wenn du genau hinschaust, siehst du, dass das Bild ein Konzert darstellt. Es gibt einen schwarzen Klavierflügel und die Zuschauer, die gespannt zuhören. In den Skizzen sieht man die sitzenden Zuhörer noch deutlicher. Es gibt auch eine weiße Säule im Bild, die wie der Klang der Musik aussieht.
+        Das Besondere an diesem Bild ist der "gelbe Klang". Das Gelb steht für die Musik und lässt das Bild wie eine Symphonie aussehen. Kandinsky hat die Farbe Gelb mit Musik in Verbindung gebracht. Er hat sogar ein Stück mit dem Titel "Der gelbe Klang" geschrieben.
+        Dieses Bild von Kandinsky zeigt, wie die Sinne zusammenarbeiten können. In seinem Buch "Über das Geistige in der Kunst" hat er erklärt, welche Farben zu welchen Tönen passen. Für ihn ist Malerei wie Musik, weil beides aus abstrakten Ideen entsteht. Er denkt, dass Malerei und Musik "absolute" Werke schaffen können, die wie von selbst entstehen`,
+      },
+      {
+        title: "Bildnis des Tänzers Alexander Sacharoff",
+        artist: "Alexej von Jawlensky",
+        date: "1909",
+        img: "https://www.lenbachhaus.de/fileadmin/_processed_/c/4/csm_G_13388__JawlenskyA_ONLINE_135c3a728a.jpg",
+        description: `Schau dir dieses wunderbare Bild an! Es zeigt den Tänzer Alexander Sacharoff, der mit seinen weit geöffneten Augen direkt in die Kamera blickt. Sein rotes Kleid und sein lächelnder Mund ziehen sofort unsere Aufmerksamkeit auf sich. Der Künstler, Alexej Jawlensky, hat den Tänzer mit schnellen und schwungvollen Linien gemalt. Kannst du die Energie und Lebendigkeit in diesem Bild spüren?
+
+        Es ist erstaunlich zu wissen, dass dieses Porträt in weniger als einer halben Stunde entstanden ist! Der Tänzer Sacharoff besuchte Jawlensky im Atelier, als er bereits für seinen Auftritt geschminkt und kostümiert war. Jawlensky malte dieses Bild ganz spontan und Sacharoff war so beeindruckt, dass er es sofort mitnahm, um sicherzugehen, dass es nicht weiter überarbeitet wird. Diese Entscheidung hat dazu beigetragen, dass das Bild so frisch und lebendig wirkt.
+        
+        Schau dich um und achte auf die einzigartigen Merkmale der Menschen um dich herum. Jeder von uns hat etwas Besonderes an sich, genau wie der Tänzer Sacharoff. Manchmal können wir männliche und weibliche Eigenschaften in einer Person finden, und das macht sie noch interessanter und schöner. Jawlensky hat das erkannt und versucht, diese Einzigartigkeit in seinen späteren Gemälden auszudrücken.
+        
+        Lass dich von diesem Bild inspirieren, deine eigenen einzigartigen Merkmale zu entdecken und zu schätzen. Sei mutig und zeige der Welt, wer du bist! Du kannst deine Kreativität zum Ausdruck bringen, genau wie Jawlensky es mit seinen schnellen Pinselstrichen getan hat. Und denke immer daran, dass wahre Schönheit in der Individualität liegt.
+        
+        Jetzt bist du an der Reihe, deine eigene Kunst zu erschaffen! Male ein Bild von dir selbst oder von jemandem, den du bewunderst. Zeige deine Persönlichkeit und deinen einzigartigen Stil. Werde zum Künstler und entdecke die Freude, die das Malen und Gestalten mit sich bringt!
+        
+        Viel Spaß beim Malen und Entdecken deiner eigenen Einzigartigkeit!`,
       },
     ],
   },
